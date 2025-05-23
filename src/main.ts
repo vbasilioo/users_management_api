85333264f6f26a18e443464a59c4287ec669307f
vbasilioo <vbasilio2019@gmail.com>
2025-05-23 18:52:10 -0300

    chore(bootstrap): remove promise test database

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  await new Promise(resolve => setTimeout(resolve, 3000));
-  
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
